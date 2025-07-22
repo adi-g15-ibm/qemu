8864d61aa813f3c38275f07fe14a3222bab3f157
Aditya Gupta <adityag@linux.ibm.com>
2025-07-22 23:10:11 +0530

    fix: Fix build error with CONFIG_POWERNV disabled
    Currently when CONFIG_POWERNV is not enabled, the build fails, such as
    with --without-default-devices:
    
        $ ./configure --without-default-devices
        $ make
    
        [281/283] Linking target qemu-system-ppc64
        FAILED: qemu-system-ppc64
        cc -m64 @qemu-system-ppc64.rsp
        /usr/bin/ld: libqemu-ppc64-softmmu.a.p/target_ppc_misc_helper.c.o: in function `helper_load_sprd':
        .../target/ppc/misc_helper.c:335:(.text+0xcdc): undefined reference to `pnv_chip_find_core'
        /usr/bin/ld: libqemu-ppc64-softmmu.a.p/target_ppc_misc_helper.c.o: in function `helper_store_sprd':
        .../target/ppc/misc_helper.c:375:(.text+0xdf4): undefined reference to `pnv_chip_find_core'
        collect2: error: ld returned 1 exit status
        ...
    
    This is since target/ppc/misc_helper.c references PowerNV specific
    'pnv_chip_find_core' call.
    
    Split the PowerNV specific SPRD code out of the generic PowerPC code, by
    moving the SPRD code to pnv.c
    
    Note that while moving the code, the 'target_ulong' type for sprc has been
    modified to 'uint64_t' since sprd is only available on 64-bit arch.
    
    Fixes: 9808ce6d5cb ("target/ppc: Big-core scratch register fix")
    Cc: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reported-by: Thomas Huth <thuth@redhat.com>
    Suggested-by: Cédric Le Goater <clg@redhat.com>
    Signed-off-by: Aditya Gupta <adityag@linux.ibm.com>

--- target/ppc/cpu.h
+++ target/ppc/cpu.h
@@ -1522,6 +1522,10 @@ struct PowerPCCPUClass {
     void (*init_proc)(CPUPPCState *env);
     int  (*check_pow)(CPUPPCState *env);
     int  (*check_attn)(CPUPPCState *env);
+
+    /* Handlers to be set by the machine initialising the chips */
+    uint64_t (*handle_sprd_load)(CPUPPCState *env);
+    void (*handle_sprd_store)(CPUPPCState *env, uint64_t val);
 };
 
 static inline bool ppc_cpu_core_single_threaded(CPUState *cs)
